212615887ad0055469467fa4ca28ba0b71a8bd1b
Adnan TURK <trkadn@gmail.com>
2021-05-09 00:24:24 +0300

    EF Progress

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -8,18 +8,31 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+            (Configuration.GetConnectionString("PostgreSqlConnection")));
+
             services.AddControllers();
             services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -34,10 +47,6 @@ namespace CommandAPI
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                // endpoints.MapGet("/", async context =>
-                // {
-                //     await context.Response.WriteAsync("Hello World! Adnan TÜRK");
-                // });
             });
         }
     }
